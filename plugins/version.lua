7b66afeb43c179595b9c6c8825fb1bb6d881ea91
botnex1122 <botnex1122@gmail.com>
2015-12-09 19:03:55 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,11 +2,10 @@ do
 
 function run(msg, matches)
   return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/vO30o
-  GNU GPL v2 license.
-  @silenceangel for more info.]]
+  بات نكس مدير گروه شما مدير:
+@amir_ab_1.]]
 end
-
+ 
 return {
   description = "Shows bot version", 
   usage = "!version: Shows bot version",
